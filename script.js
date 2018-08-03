848a4e14d9e731002458b42ea430b7ba1afb034c
Guillermo Aguilar <acguiller@gmail.com>
2018-08-04 00:30:10 +0200

    adding visual and focus

--- script.js
+++ script.js
@@ -1,41 +1,67 @@
 window.addEventListener('keydown', playSound)
 
-let column = [[],[],[],[]] //4*4
+let sound = [[],[],[],[]] //4*4
 
-window.addEventListener('keydown', function(e){
-  if (column[0].length < 4){
-    column[0].push(e.keyCode)
-  } else if(column[1].length < 4){
-    column[1].push(e.keyCode)
-  } else if(column[2].length < 4){
-    column[2].push(e.keyCode)
-  } else if(column[3].length < 4){
-    column[3].push(e.keyCode)
-  } else return
-})
+window.addEventListener('keydown', populateSounds)
 
 const playButton = document.querySelector('.play')
 playButton.addEventListener('click', playRecord);
-let index = 0;
+
+
+
+function populateSounds (e){
+  if (sound[0].length == 0){
+    sound[0].push(e.keyCode)
+  } else if(sound[1].length == 0){
+    sound[1].push(e.keyCode)
+  } else if(sound[2].length == 0){
+    sound[2].push(e.keyCode)
+  } else if(sound[3].length == 0){
+    sound[3].push(e.keyCode)
+  } else return
+}
+
+
 
 function playRecord (){
   var playInterval = setInterval(playColumn, 400); //Play column 0.4s rate
 }
 
+
+let index = 0;//counter for looping throung groups of sounds(columns/arrays inside sound array)
 function playColumn (){ //loop through the column sound.
-    if(index > 3) index = 0;
-    column[index].map((k)=>{
+    if(index > 3) index = 0; //reset the loop
+    sound[index].map((k)=>{
     const aud = document.querySelector(`audio[data-key="${k}"]`)
     aud.currentTime = 0;
     aud.play();
     })
-    index++
+    index++ //To go next group of sound/column
     console.log(index);
 }
+
+
+
+
 
 function playSound(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   if(!audio) return; //stop the function from running.
   audio.currentTime = 0; //reset sound.
-  audio.play()
+  audio.play();
+  drawFocus();
+}
+let position = 1;// position of focus (red border!)
+
+function drawFocus(){//Draw focus + fill if sound in.
+  if (position > 1){//if position =! first position calculate last position to remove class 'active'
+    let lastPosition = position -1
+    const lastBloque = document.getElementById(lastPosition);
+    lastBloque.classList.remove('active')
+  }
+
+  const bloque = document.getElementById(position)
+  bloque.classList.add('active')
+  bloque.classList.add('filled')
+  position++
 }
