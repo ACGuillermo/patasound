6df20cf4b8cb735902c5b7147596c46d95bfc676
Guillermo Aguilar <acguiller@gmail.com>
2018-08-05 00:58:47 +0200

    fixed focus and populate sound

--- script.js
+++ script.js
@@ -1,26 +1,85 @@
-window.addEventListener('keydown', playSound)
-
-let sound = [[],[],[],[]] //4*4
+let sound = [[32,32,32,32],[32,32,32,32],[32,32,32,32],[32,32,32,32]] //4*4
 
 window.addEventListener('keydown', populateSounds)
+window.addEventListener('keydown', playSound)
+
 
 const playButton = document.querySelector('.play')
 playButton.addEventListener('click', playRecord);
 
 
-let totalSounds = 0 //Counter for total sounds. Max sounds in a column = 4(sound[i].length = 4)
 function populateSounds (e){
-  if (totalSounds == 0 || totalSounds == 4 || totalSounds == 8 || totalSounds == 12){
-    sound[0].push(e.keyCode)
-  } else if (totalSounds == 1 || totalSounds == 5 || totalSounds == 9 || totalSounds == 13){
-    sound[1].push(e.keyCode)
-  } else if(totalSounds == 2 || totalSounds == 6 || totalSounds == 10 || totalSounds == 14){
-    sound[2].push(e.keyCode)
-  }else{
-    sound[3].push(e.keyCode)
-  }
+  console.log(e.keyCode);
+    let auxPos = 0
+  switch(position){
+      case 1:
+        auxPos = 0
+        sound[0][0] = e.keyCode
+        break;
+      case 5:
+        auxPos = 1
+        sound[0][1] = e.keyCode
+        break;
+      case 9:
+        auxPos = 2
+        sound[0][2] = e.keyCode
+        break;
+      case 13:
+        auxPos = 3
+        sound[0][3] =e.keyCode
+        break;
 
-  totalSounds++
+      case 2:
+        auxPos = 0;
+        sound[1][0] = e.keyCode
+        break;
+      case 6:
+        auxPos = 1;
+        sound[1][1] = e.keyCode
+        break;
+      case 10:
+        auxPos = 2;
+        sound[1][2] = e.keyCode
+        break;
+      case 14:
+        auxPos = 3;
+        sound[1][3] = e.keyCode
+        break;
+
+      case 3:
+        auxPos = 0;
+        sound[2][0] = e.keyCode
+        break;
+      case 7:
+        auxPos = 1;
+        sound[2][1] = e.keyCode
+        break;
+      case 11:
+        auxPos = 2;
+        sound[2][2] = e.keyCode
+        break;
+      case 15:
+        auxPos = 3;
+        sound[2][3] = e.keyCode
+        break;
+
+      case 4:
+        auxPos = 0;
+        sound[3][0] = e.keyCode
+        break;
+      case 8:
+        auxPos = 1;
+        sound[3][1] = e.keyCode
+        break;
+      case 12:
+        auxPos = 2;
+        sound[3][2] = e.keyCode
+        break;
+      case 16:
+        auxPos = 3;
+        sound[3][3] = e.keyCode
+        break;
+    }
 }
 
 
@@ -45,18 +104,18 @@ function playColumn (){ //loop through the column sound.
 
 
 function drawColumn (i){//Drawing columns when looping through them
-  let indexColumn = [[1,5,9,13],[2,6,10,14],[3,7,11,15],[4,8,12,16]]
+  let indexColumn = [[1,5,9,13],[2,6,10,14],[3,7,11,15],[4,8,12,16]] // Columns matching Ids in html
   if(i>0){
     let aux = i - 1;
-    indexColumn[aux].map((id)=>{
+    indexColumn[aux].map((id)=>{//remove active from last column
       const auxBox = document.getElementById(id);
       auxBox.classList.remove('active')
     })
   }
-  indexColumn[3].map((id)=>{
+  indexColumn[3].map((id)=>{//remove active from 4th column
     const auxBox = document.getElementById(id);
     auxBox.classList.remove('active')
-  })
+  }) //Draw active current column
   indexColumn[i].map((id)=>{
     const box = document.getElementById(id)
     box.classList.add('active')
@@ -65,23 +124,82 @@ function drawColumn (i){//Drawing columns when looping through them
 
 
 function playSound(e){
+  prevPosition = position;
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   if(!audio) return; //stop the function from running.
   audio.currentTime = 0; //reset sound.
   audio.play();
-  drawFocus();
+  if (position == 16){
+    return
+  }
+  position++
+  drawFocus(prevPosition);
 }
-let position = 1;// position of focus (red border!)
 
-function drawFocus(){//Draw focus + fill if sound in.
-  if (position > 1){//if position =! first position calculate last position to remove class 'active'
-    let lastPosition = position -1
-    const lastBloque = document.getElementById(lastPosition);
-    lastBloque.classList.remove('active')
+let position = 1;// position of focus (red border!)
+let prevPosition = 1;//last position of focus
+
+window.addEventListener('keydown', moveFocus)
+
+function moveFocus(e){ //Move focus with arrow keys
+  prevPosition = position;
+  let move = e.keyCode;
+  switch (move) {
+    case 38: //up
+      if(position == 1 || position == 2 || position == 3 || position == 4){
+        position += 12;
+        drawFocus(prevPosition);
+      } else{
+        position -= 4;
+        drawFocus(prevPosition);
+        }
+      break;
+    case 37: //left
+      if(position == 1 || position == 5 || position == 9 || position == 13){
+        position += 3;
+        drawFocus(prevPosition);
+      } else{
+        position -= 1;
+        drawFocus(prevPosition);
+        }
+      break;
+    case 40: //down
+      if (position == 13 || position == 14 || position == 15 || position == 16) {
+        position -= 12;
+        drawFocus(prevPosition);
+      } else{
+        position += 4;
+        drawFocus(prevPosition)
+        }
+      break;
+    case 39://right
+      if (position == 4 || position == 8 || position == 12 || position == 16){
+        position -= 3;
+        drawFocus(prevPosition);
+      } else{
+        position += 1;
+        drawFocus(prevPosition)
+        }
+      break;
   }
 
-  const bloque = document.getElementById(position)
-  bloque.classList.add('active')
-  bloque.classList.add('filled')
-  position++
+}
+
+function drawFocus(lastPos){//Draw focus
+  const lastBlock = document.getElementById(lastPos);
+  lastBlock.classList.remove('active')
+  const block = document.getElementById(position)
+  block.classList.add('active')
+
+
+  // if (position > 1){//if position =! first position calculate last position to remove class 'active'
+  //   let lastPosition = position -1
+  //   const lastBloque = document.getElementById(lastPosition);
+  //   lastBloque.classList.remove('active')
+  // }
+  //
+  // const bloque = document.getElementById(position)
+  // bloque.classList.add('active')
+  // bloque.classList.add('filled')
+  // position++
 }
